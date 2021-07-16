69384e6ed9b03d0aa9ed49a29e9b68b0144eb944
Joshua Audrey Mudanyi <joshuaaudreymudanyi@gmail.com>
2021-07-16 14:36:10 +0300

    an empty message

--- express/preview.js
+++ express/preview.js
@@ -55,86 +55,6 @@ function preview(request, response)
     body += "</div>";
     body += "</div>";
     
-    if(global.project.clips.length !== 0)
-    {
-    	body += "<div style='display:block; text-align:center; border-style:solid; border-color:black; border-radius:2.5px; width:350px; height:75px; margin-top:15px;'>";
-    	body += "<div style='display:table; width:100%; height:100%;'>";
-    	body += "<div style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
-    	body += "These are the extracted clips for the project "+project.name+".";
-    	body += "</div>";
-    	body += "</div>";
-    	body += "</div>";
-    	
-    	body += "<script>";
-    	body += "function persist(clip) { var url = '{{root}}/persist/'+encodeURIComponent(clip); window.location = url; }";
-    	body += "</script>";
-    }
-    
-    body += "<div style='display:block;";
-    if(global.project.clips.length > 2)
-    {
-    	body += " width: "+global.project.clips.length*365+"px;";
-    }
-    body += " margin-right:15px; margin-bottom: 15px;'>";
-    global.project.clips.forEach(function(clip)
-    {
-    	body += "<div style='float:left; margin-right:15px;'>";
-    	
-    	body += "<img src='{{root}}/"+clip.frame.begin+"' alt='{{root}}/"+clip.frame.begin+" not loaded.' style='display:block; margin-top:15px; width:350px; height:auto;'>";
-    	body += "<div style='display:block; text-align:left; margin-top:15px;'>";
-    	body += "<span style='color:red;'>Begin:</span> "+clip.begin;
-    	body += "</div>";
-    	
-    	body += "<img src='{{root}}/"+clip.frame.end+"' alt='{{root}}/"+clip.frame.end+" not loaded.' style='display:block; margin-top:15px; width:350px; height:auto;'>";
-    	body += "<div style='display:block; text-align:left; margin-top:15px;'>";
-    	body += "<span style='color:red;'>End:</span> "+clip.end;
-    	body += "</div>";
-    	
-    	body += "<div style='display:block; margin-top:15px; text-align:center; border-radius:2.5px; background-color:red; width:350px; height:75px;'>";
-    	body += "<div style='display:table; width:100%; height:100%;'>";
-    	body += "<a href='{{root}}/"+clip.name+"' download style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
-    	body += "Download "+clip.name+".";
-    	body += "</a>";
-    	body += "</div>";
-    	body += "</div>";
-    	
-    	body += "<div onClick='";
-    	if(clip.persist === true)
-    	{
-    		body += "javascript:void(0);";
-    	}
-    	else
-    	{
-    		body += "persist(\""+clip.name+"\")";
-    	}
-    	body += "' style='display:block; margin-top:15px; text-align:center; border-radius:2.5px; ";
-    	if(clip.persist === true)
-    	{
-    		body += "background-color:blue; ";
-    	}
-    	else
-    	{
-    		body += "background-color:red; ";
-    	}
-    	body += "color:white; width:350px; height:75px;'>";
-    	body += "<div style='display:table; width:100%; height:100%;'>";
-    	body += "<div style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
-    	if(clip.persist === true)
-    	{
-    		body += "The clip "+clip.name+" with begin "+clip.begin+" and end "+clip.end+" has been persisted to mongodb.";
-    	}
-    	else
-    	{
-    		body += "Persist the clip "+clip.name+" with begin "+clip.begin+" and end "+clip.end+" to mongodb.";
-    	}
-    	body += "</div>";
-    	body += "</div>";
-    	body += "</div>";
-    	
-    	body += "</div>";
-    });
-    body += "</div>";
-    
     body += "<div style='display:block; text-align:center; border-style:solid; border-color:black; border-radius:2.5px; width:350px; height:75px; margin-top:15px;'>";
     body += "<div style='display:table; width:100%; height:100%;'>";
     body += "<div style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
@@ -204,6 +124,86 @@ function preview(request, response)
     	body += "</div>";
     	body += "</div>";
     });
+    
+    if(global.project.clips.length !== 0)
+    {
+    	body += "<div style='display:block; text-align:center; border-style:solid; border-color:black; border-radius:2.5px; width:350px; height:75px; margin-top:15px;'>";
+    	body += "<div style='display:table; width:100%; height:100%;'>";
+    	body += "<div style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
+    	body += "These are the extracted clips for the project "+project.name+".";
+    	body += "</div>";
+    	body += "</div>";
+    	body += "</div>";
+    
+    	body += "<script>";
+    	body += "function persist(clip) { var url = '{{root}}/persist/'+encodeURIComponent(clip); window.location = url; }";
+    	body += "</script>";
+    }
+    
+    body += "<div style='display:block;";
+    if(global.project.clips.length > 2)
+    {
+    	body += " width:"+global.project.clips.length*365+"px;";
+    }
+    body += " margin-right:15px; margin-bottom:15px;'>";
+    global.project.clips.forEach(function(clip)
+    {
+    	body += "<div style='float:left; margin-right:15px;'>";
+    
+    	body += "<img src='{{root}}/"+clip.frame.begin+"' alt='{{root}}/"+clip.frame.begin+" not loaded.' style='display:block; margin-top:15px; width:350px; height:auto;'>";
+    	body += "<div style='display:block; text-align:left; margin-top:15px;'>";
+    	body += "<span style='color:red;'>Begin:</span> "+clip.begin;
+    	body += "</div>";
+    
+    	body += "<img src='{{root}}/"+clip.frame.end+"' alt='{{root}}/"+clip.frame.end+" not loaded.' style='display:block; margin-top:15px; width:350px; height:auto;'>";
+    	body += "<div style='display:block; text-align:left; margin-top:15px;'>";
+    	body += "<span style='color:red;'>End:</span> "+clip.end;
+    	body += "</div>";
+    
+    	body += "<div style='display:block; margin-top:15px; text-align:center; border-radius:2.5px; background-color:red; width:350px; height:75px;'>";
+    	body += "<div style='display:table; width:100%; height:100%;'>";
+    	body += "<a href='{{root}}/"+clip.name+"' download style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
+    	body += "Download "+clip.name+".";
+    	body += "</a>";
+    	body += "</div>";
+    	body += "</div>";
+    
+    	body += "<div onClick='";
+    	if(clip.persist === true)
+    	{
+    		body += "javascript:void(0);";
+    	}
+    	else
+    	{
+    		body += "persist(\""+clip.name+"\")";
+    	}
+    	body += "' style='display:block; margin-top:15px; text-align:center; border-radius:2.5px; ";
+    	if(clip.persist === true)
+    	{
+    		body += "background-color:blue; ";
+    	}
+    	else
+    	{
+    		body += "background-color:red; ";
+    	}
+    	body += "color:white; width:350px; height:75px;'>";
+    	body += "<div style='display:table; width:100%; height:100%;'>";
+    	body += "<div style='display:table-cell; vertical-align:middle; width:100%; height:100%'>";
+    	if(clip.persist === true)
+    	{
+    		body += "The clip "+clip.name+" with begin "+clip.begin+" and end "+clip.end+" has been persisted to mongodb.";
+    	}
+    	else
+    	{
+    		body += "Persist the clip "+clip.name+" with begin "+clip.begin+" and end "+clip.end+" to mongodb.";
+    	}
+    	body += "</div>";
+    	body += "</div>";
+    	body += "</div>";
+    
+    	body += "</div>";
+    });
+    body += "</div>";
     //
     var html = global.base;
     //
